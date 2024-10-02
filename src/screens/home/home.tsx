ebaa4a784f9ef49491c4e20bd232d580a6747781
João Rhoden <j.machado02@edu.pucrs.br>
2024-10-02 00:01:33 -0300

    add title to home

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,16 +1,20 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { AuthContext } from "../../context/authContext";
 import '../../styles/Home.css';
 
 export const Home = () => {
   const authContext = useContext(AuthContext);
 
+  useEffect(() => {
+    document.title = 'Home - My App';
+  }, []);
+
   if (!authContext || !authContext.user) {
     return <div>Loading...</div>; 
   }
   return (
     <div className="container">
-      <p>Home</p>
+      
     </div>
   );
 };
